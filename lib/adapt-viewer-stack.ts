a1fc33eff30206a902a11f09a3024ad7b08549b9
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-02-17 11:32:32 -0500

    fix committed merge conflict

--- lib/adapt-viewer-stack.ts
+++ lib/adapt-viewer-stack.ts
@@ -126,11 +126,7 @@ export class AdaptViewerStack extends cdk.Stack {
                 })
               ]
             }),
-<<<<<<< HEAD
-            cache: { enabled: true, ttl: Duration.minutes(5) }
-=======
             //cache: { enabled: true, ttl: Duration.minutes(5) }
->>>>>>> upstream/main
           }
         },
         "/reports/{slug}": {
